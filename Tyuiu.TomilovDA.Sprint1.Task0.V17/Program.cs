2a33d9afaa8f54979fe2dba39cf40096903f7744
TheJouster <tomilovdima999@gmail.com>
2024-09-15 19:05:01 +0500

    Task0.1

--- Tyuiu.TomilovDA.Sprint1.Task0.V17/Program.cs
+++ Tyuiu.TomilovDA.Sprint1.Task0.V17/Program.cs
@@ -18,19 +18,20 @@ namespace Tyuiu.TomilovDA.Sprint1.Task0.V17
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
             Console.WriteLine("* Написать программу, которая вычисляет выражение 9/3*2+12/4/3-2          *");
-            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("9 / 3 * 2 + 12 / 4 / 3 - 2");
+            Console.Write("9 / 3 * 2 + 12 / 4 / 3 - 2");
+            Console.WriteLine();
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate());
+            Console.Write(ds.Calculate());
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
