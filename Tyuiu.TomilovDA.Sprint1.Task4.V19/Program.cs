4e44be0b6ac08576e0b2b4ad1299a8b042cbbb6f
TheJouster <tomilovdima999@gmail.com>
2024-09-25 20:45:07 +0500

    Task4

---
+++ Tyuiu.TomilovDA.Sprint1.Task4.V19/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.TomilovDA.Sprint1.Task4.V19.Lib;
+namespace Tyuiu.TomilovDA.Sprint1.Task4.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Class Math                                                              *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране. Ответ округлите*");
+            Console.WriteLine("* до 3 знаков после запятой.                                              *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Введите значение Y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(x,y);
+            Console.WriteLine("(x+y)/|x-2| = "+result);
+            Console.ReadKey();
+        }
+    }
+}
