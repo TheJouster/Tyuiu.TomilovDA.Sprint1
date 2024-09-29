4a44e7513b6ad6c1ebbbd84cd14f3827c4e0b655
TheJouster <tomilovdima999@gmail.com>
2024-09-29 22:11:06 +0500

    Task6.1

--- Tyuiu.TomilovDA.Sprint1.Task6.V8.Lib/DataService.cs
+++ Tyuiu.TomilovDA.Sprint1.Task6.V8.Lib/DataService.cs
@@ -6,7 +6,17 @@ namespace Tyuiu.TomilovDA.Sprint1.Task6.V8.Lib
     {
         public string MoveLetterToEnd(string value)
         {
-            return value;
+            string res = "";
+            string[] text = value.Split(' ');
+            for (int i = 0; i < text.Length; i++)
+            {
+                string word = text[i];
+                char chr = text[i][0];
+                word = word.Remove(0,1);
+                res += word + chr + " ";
+            }
+            res = res.Trim();
+            return res;
         }
     }
 }
